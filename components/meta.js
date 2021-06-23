331094866b6ada73fe44dcf4788ee30b014ef2d7
Baroque Engine <rsakane+github@gmail.com>
2021-06-24 08:27:49 +0900

    updated.

--- components/meta.js
+++ components/meta.js
@@ -3,10 +3,6 @@ import Head from "next/head";
 export default function Meta() {
   return (
     <Head>
-      <meta
-        name="description"
-        content={`グラフィックスプログラミング関連技術についての覚え書き`}
-      />
       <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/katex@0.13.3/dist/katex.min.css"
